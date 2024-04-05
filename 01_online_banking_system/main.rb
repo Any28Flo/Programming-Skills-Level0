e3354c8a52a7f037f8f7d68d6e0739b1320b2ac7
ErikaFlores <eflores@dumaxst.com>
2024-04-04 22:06:04 -0600

    feature|finish challenge banking system

--- 01_online_banking_system/main.rb
+++ 01_online_banking_system/main.rb
@@ -5,44 +5,69 @@
 1.2 If the user enters the wrong credentials three times, the system must lock them out.
 2.0 The initial balance in the bank account is $2000.
 2.1 The system must allow users to deposit, withdraw, view, and transfer money.
-2.2 The system must display a menu for users to perform transactions.   
+2.2 The system must display a menu for users to perform transactions.  
 =end
 
 
 
 number_attemps = 0;
-validPassword = "winterIsComming"
+validPassword = "123"
 validUsername = "batman"
 initialValance = 2000
 flag = true
-
-
+flagExit = true
+max_attemps = 2
    
-    while flag && number_attemps < 3
+    while flag
         print "What is your username? "
         username = gets.chomp
         print "What is your password? "
         password = gets.chomp
-        
-        if number_attemps <3 
+        print(number_attemps)
+        if  password == validPassword && username == validUsername
+            
+            while flagExit
+
+                puts "Welcome"
+                puts "1.Deposit"
+                puts "2.withdraw"
+                puts "3. view"
+                puts "4. transfer"
+                puts "5. exit"
+                
+                option = gets.chomp.to_i
+                    if option == 1
+                        puts "Insert the ammount to insert"
+                        ammount = gets.chomp
+                        initialValance += ammount.to_i
+                       
+                    elsif option == 2
+                        puts "insert the amount to withdraw"
+                        withdraw = gets.chomp
+                        initialValance -= withdraw.to_i
+                    elsif option == 3
+                        puts "Your total is #{initialValance}"
+                    elsif option == 4
+                        puts "Account to transfer: "
+                        account = gets.chomp
+                        puts "Amount to transfer: "
+                        amount = gets.chomp
+                        initialValance -= amount.to_i
+
+                    elsif option == 5
+                        print "THANKS FOR USING OUR SERVICES"
+                        flagExit = false
+                        flag = false
+                            
+                    end
+            
+            end
+           
+        elsif number_attemps == max_attemps
+            "You have reached the maximum number of attemps, please try again later"
+            flag = false
+        else 
             number_attemps +=1
-        else
-            flag =false
         end
        
-        if  password == validPassword && username == validUsername
-            puts "Welcome"
-            
-        else
-            puts "Wrong credentials"
-           
-          
-        end
     end
-
-   
-
-
-puts username
-puts password
-puts number_attemps
